58e78e94d553507ede496cb5433d65b01be279e4
S-kkipie <issacysofia@gmail.com>
2024-03-12 15:04:24 -0500

    homepage

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
-  base: "/S-kkipie.github.io/",
+  base: "/Pokedex/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
